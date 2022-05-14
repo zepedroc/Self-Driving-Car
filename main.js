f794d1c6ad8df64324918398081a6a01289e723d
zepedroc <zepedrocm@hotmail.com>
2022-05-14 16:22:45 +0100

    Create Road

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const canvas = document.getElementById('myCanvas');
 canvas.window = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 car.draw(ctx)
 
 animate();
@@ -13,6 +14,15 @@ function animate() {
 
     // by setting a new height for the canvas, it resets and cleans the previous drawings
     canvas.height = window.innerHeight;
+
+    ctx.save();
+
+    // move the map instead of the car
+    ctx.translate(0, -car.y + canvas.height * 0.7);
+
+    road.draw(ctx)
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
---
+++ utils.js
@@ -0,0 +1,4 @@
+// linear interpolation function
+function lerp(A, B, t) {
+    return A + (B - A) * t;
+}
